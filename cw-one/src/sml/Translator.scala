42d0d351eb93af9046d3377bf116a2b87c6a1fbb
rossanthony <ross@velo33.com>
2016-03-13 20:42:38 +0000

    cw-one: Adding the "mul" instruction subclass

--- cw-one/src/sml/Translator.scala
+++ cw-one/src/sml/Translator.scala
@@ -6,6 +6,7 @@ package sml
 class Translator(fileName: String) {
   private final val ADD = "add"
   private final val DIV = "div"
+  private final val MUL = "mul"
   private final val LIN = "lin"
   private final val OUT = "out"
 
@@ -30,6 +31,8 @@ class Translator(fileName: String) {
             program = program :+ AddInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case DIV =>
             program = program :+ DivInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
+          case MUL =>
+            program = program :+ MulInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case LIN =>
             program = program :+ LinInstruction(fields(0), fields(2).toInt, fields(3).toInt)
           case OUT =>
