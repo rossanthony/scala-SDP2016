5c6c27b629093db005d21da1c263ba5952f6c5bb
rossanthony <ross@velo33.com>
2016-03-25 15:06:06 +0000

    cw-one: Reverting the Machine class back to original impl

--- cw-one/sml/src/main/scala/Machine.scala
+++ cw-one/sml/src/main/scala/Machine.scala
@@ -15,41 +15,8 @@ case class Machine(labels: Labels, prog: Vector[Instruction]) {
 
   // Execute the program in prog, beginning at instruction 0.
   // Precondition: the program and its labels have been store properly.
-/*
   def execute(start: Int) =
     start.until(prog.length).foreach(x => prog(x) execute this)
-  */
-
-  // The foreach method above has been replaced with a while loop, because it was causing issues with the Bnz jump to
-  // line calls, e.g. the following SML...
-  //    f0 lin 0 6
-  //    f1 lin 1 1
-  //    f2 lin 2 1
-  //    f3 mul 1 1 0
-  //    f4 sub 0 0 2
-  //    f5 bnz 0 f3
-  //    f6 out 1
-  //
-  // Results in:
-  //    Beginning program execution.
-  //    720
-  //    720
-  //    720
-  //    720
-  //    720
-  //    720
-  //    Ending program execution.
-  //
-  // Although arguably less elegant, I found that using a while loop instead allows jumping to previous points
-  // in the loop, without repeating the last func call to `f6 out 1` five too many times!
-
-  def execute(start: Int): Unit = {
-    currentLn = start
-    while (currentLn <= prog.length - 1) {
-      prog(currentLn) execute this
-      currentLn += 1
-    }
-  }
 }
 
 object Machine extends App {
